2cb72e8348c936e4a0724a22b0a57ed596e66fb2
noriakis <nori@hgc.jp>
2023-08-25 15:50:35 +0900

    add new files

---
+++ tests/testthat/test-pathway.R
@@ -0,0 +1,15 @@
+test_that("Generate test pathway without errors", {
+	expect_error( create_test_pathway(), NA)
+})
+test_that("Pathway downloading without errors", {
+	expect_error( pathway("hsa04110"), NA)
+})
+test_that("process_line without errors", {
+	expect_error( create_test_pathway(line=TRUE) |> process_line(), NA)
+})
+test_that("process_reaction without errors", {
+	expect_error( create_test_pathway(line=TRUE) |> process_reaction(), NA)
+})
+test_that("ggkegg (pathway) without errors", {
+	expect_error( ggkegg("hsa04110"), NA)
+})
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,23 @@
+test_that("Do utils without errors", {
+    graph <- create_test_pathway()
+    res <- data.frame(row.names="6737",log2FoldChange=1.2)
+    expect_error( graph |>
+        mutate(num=assign_deseq2(res, gene_type="ENTREZID")),
+        NA)
+    expect_error( graph |> activate("edges") |>
+        mutate(num=edge_numeric_sum(c(1.2,-1.2) |>
+                                    setNames(c("TRIM21","DDX41")),
+                                    name="graphics_name")),
+        NA)
+})
+
+test_that("edge_matrix without errors", {
+	graph <- create_test_pathway()
+    num_df <- data.frame(row.names=c("6737","51428"),
+                    "sample1"=c(1.1,1.2),
+                    "sample2"=c(1.1,1.2),
+                     check.names=FALSE)
+    expect_error(graph <- graph |> edge_matrix(num_df, gene_type="ENTREZID"),
+    	NA)                     	
+})
+
